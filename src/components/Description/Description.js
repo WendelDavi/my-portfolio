9fac9356c198be9706b725fea62d8cba2286cfcd
Wendel <wendeldavi.0424@aluno.saojudas.br>
2024-06-11 18:30:48 -0300

    Adição de foto na descrição.

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Description.module.css';
+import myPhoto from '../../assets/my-photo.jpg'
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Description = () => (
     <main>
         <div className={styles.container} id="inicio">
+            <div className={styles.photoContainer}>
+                <img src={myPhoto} alt="Minha Foto" className={styles.photo} />
+            </div>
             <h1 className={styles.title}>Wendel Silva Davi</h1>
             <p className={styles.description}>
                 Um desenvolvedor Front-End com o objetivo de aprender cada vez mais através da criação de projetos.
