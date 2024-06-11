3792a90547dd0bdb2d5b9d88a29627a892d93388
Wendel <wendeldavi.0424@aluno.saojudas.br>
2024-06-11 18:48:17 -0300

    Mudança nos projetos.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,14 +6,8 @@ const Projects = () => (
         <h2 className={styles.title}>Projetos</h2>
         <div className={styles.grid}>
             <div className={styles.project}>
-                <h3>Tabuada</h3>
-                <p>Um site onde o usuário digita um número, e é exibido a tabuada desse número.</p>
-                <a href="https://tabuada-nxkm9h90u-wendel-davis-projects.vercel.app" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-                <a href="https://github.com/WendelDavi/wendeldavi-portfolio/tree/main/tabuada" target="_blank" rel="noopener noreferrer">Ver Repositório</a>
-            </div>
-            <div className={styles.project}>
                 <h3>Lista ToDo</h3>
-                <p>Um aplicativo de lista de tarefas, onde o usuário pode adicionar, remover, editar e marcar tarefas como concluídas.</p>
+                <p>Um site de lista de tarefas, onde o usuário pode adicionar, remover, editar e marcar tarefas como concluídas.</p>
                 <a href="https://todo-list-git-main-wendel-davis-projects.vercel.app" target="_blank" rel="noopener norefer">Ver Projeto</a>
                 <a href="https://github.com/WendelDavi/todo-list" target="_blank" rel="noopener norefer">Ver Repositório</a>
             </div>
